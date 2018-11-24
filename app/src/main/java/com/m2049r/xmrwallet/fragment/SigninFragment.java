227b9b894b9c2253b3370d3fd70ce55a3d5c955d
Fran Dioniz <frandioniz@gmail.com>
2018-11-24 10:09:14 +0000

    Google sign in
    Conflicts:
    	app/google-services.json

--- app/src/main/java/com/m2049r/xmrwallet/fragment/SigninFragment.java
+++ app/src/main/java/com/m2049r/xmrwallet/fragment/SigninFragment.java
@@ -7,17 +7,27 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.m2049r.xmrwallet.LoginActivity;
 import com.m2049r.xmrwallet.PreLoginActivity;
 import com.m2049r.xmrwallet.R;
@@ -27,9 +37,11 @@ import com.m2049r.xmrwallet.widget.ProgressDialogCV;
 public class SigninFragment extends Fragment implements View.OnClickListener {
 
     public static final String TAG = SigninFragment.class.getName();
+    private static final int RC_SIGN_IN = 9001;
 
     private Context mContext;
     private FirebaseAuth auth;
+    private GoogleSignInClient mGoogleSignInClient;
 
     private TextView signUpTV;
     private Button signInBTN;
@@ -37,6 +49,7 @@ public class SigninFragment extends Fragment implements View.OnClickListener {
     private TextView recoveryTV;
     private InputLayout emailIL, passwordIL;
     private ProgressDialogCV progressDialog;
+    private FrameLayout googleFL;
 
     public static SigninFragment newInstance() {
 
@@ -52,6 +65,14 @@ public class SigninFragment extends Fragment implements View.OnClickListener {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_signin, container, false);
         mContext = getActivity();
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(mContext, gso);
+
         auth = FirebaseAuth.getInstance();
         initComponents(rootView);
         initListeners();
@@ -67,12 +88,14 @@ public class SigninFragment extends Fragment implements View.OnClickListener {
         headerTitleTV.setText("Sign in");
         recoveryTV = rootView.findViewById(R.id.tv_recovery);
         progressDialog = new ProgressDialogCV(getActivity(), R.string.loading_message);
+        googleFL = rootView.findViewById(R.id.fl_google);
     }
 
     private void initListeners() {
         signUpTV.setOnClickListener(this);
         signInBTN.setOnClickListener(this);
         recoveryTV.setOnClickListener(this);
+        googleFL.setOnClickListener(this);
     }
 
     @Override
@@ -89,6 +112,9 @@ public class SigninFragment extends Fragment implements View.OnClickListener {
                 break;
             case R.id.tv_recovery:
                 ((PreLoginActivity) getActivity()).replaceFragment(RecoveryFragment.newInstance());
+                break;
+            case R.id.fl_google:
+                signInGoogle();
                 break;
         }
     }
@@ -130,6 +156,61 @@ public class SigninFragment extends Fragment implements View.OnClickListener {
                         } else {
                             Intent intent = new Intent(mContext, LoginActivity.class);
                             startActivity(intent);
+                        }
+                    }
+                });
+    }
+
+
+    private void signInGoogle() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                //updateUI(null);
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+        progressDialog.show();
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = auth.getCurrentUser();
+                            //updateUI(user);
+                            Intent intent = new Intent(mContext, LoginActivity.class);
+                            startActivity(intent);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(getActivity(), "Authentication failed." + task.getException(),
+                                    Toast.LENGTH_SHORT).show();
+                            //Snackbar.make(findViewById(R.id.main_layout), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                            //updateUI(null);
                         }
                     }
                 });
